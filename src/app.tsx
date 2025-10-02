246f19868d8b3b57330aef0434226bcaab90711c
misael bueno <misa.hbueno@gmail.com>
2025-10-02 16:45:41 -0300

    configs iniciais

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo-in-orbit.svg'
+
+export function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+    </div>
+  )
+}
+
+export default App
