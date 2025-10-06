3f55db08da49f58c8a434e196d840e57e24186c7
misael bueno <misa.hbueno@gmail.com>
2025-10-06 15:05:42 -0300

    add new libs

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { Plus } from 'lucide-react'
 import letsStart from './assets/lets-start-illustration.svg'
 import logo from './assets/logo-in-orbit.svg'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
@@ -11,13 +12,10 @@ export function App() {
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
 
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracing-tight hover:bg-violet-600"
-      >
+      <Button>
         <Plus className="size-4" />
         Cadastrar meta
-      </button>
+      </Button>
     </div>
   )
 }
